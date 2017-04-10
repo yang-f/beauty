d65b4d41eef29fd3416e338b906e4908680524f4
yangfei <yfei89@126.com>
2017-04-10 11:50:03 +0800

    test

--- controllers/adminController.go
+++ controllers/adminController.go
@@ -5,10 +5,10 @@ import (
 )
 
 type User struct {	
-	user_id        int     `json:"user_id"`
-	user_name      string  `json:"user_name"`
+	User_id        int     `json:"user_id"`
+	User_name      string  `json:"user_name"`
 	user_pass      string  `json:"user_pass"`
-	user_type      string  `json:"user_type"`
+	User_type      string  `json:"user_type"`
 	User_mobile    string  `json:"user_mobile"`
 	Add_time  	   string `json:"add_time"`
 }
