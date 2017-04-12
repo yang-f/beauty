4d29201d19511a96ece32d55e2fcdf428a83ceaa
yangfei <yfei89@126.com>
2017-04-12 18:06:20 +0800

    models and session manager

--- controllers/adminController.go
+++ controllers/adminController.go
@@ -4,19 +4,6 @@ import (
 	"net/http"
 )
 
-type User struct {	
-	User_id        int     `json:"user_id"`
-	User_name      string  `json:"user_name"`
-	user_pass      string  `json:"user_pass"`
-	User_type      string  `json:"user_type"`
-	User_mobile    string  `json:"user_mobile"`
-	Add_time  	   string `json:"add_time"`
-}
-
-type  Users []User
-
-var CurrentUser = User{}
-
 func Config(w http.ResponseWriter, r *http.Request) {
 	b := []byte(`{"description":"this is json"}`)
 	w.Write(b)
---
+++ sessions/sessions.go
@@ -0,0 +1,37 @@
+package sessions
+
+import(
+	"net/http"
+	"github.com/yang-f/beauty/models"
+	"github.com/yang-f/beauty/utils/token"
+    "github.com/yang-f/beauty/db"
+)
+
+func CurrentUser(r *http.Request) models.User{
+	cookie, err := r.Cookie("token")
+    if err != nil || cookie.Value == ""{
+        return models.User{};
+    }
+    user_id, err := token.Valid(cookie.Value)
+    if err != nil {
+        return models.User{};
+    }
+
+    rows, res, err := db.Query("select * from user where user_id= '%d'", user_id)
+
+    if err != nil {
+        return models.User{};
+    }
+
+    if len(rows) == 0 {
+    	return models.User{};
+    }
+    row := rows[0]
+    user := models.User{
+    		User_id:row.Int(res.Map("user_id")), 
+    		User_name:row.Str(res.Map("user_name")),
+    		User_type:row.Str(res.Map("user_type")),
+    		Add_time:row.Str(res.Map("add_time"))}
+
+	return user
+}
